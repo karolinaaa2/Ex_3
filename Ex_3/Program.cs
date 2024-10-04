5038e3b2091ab32b0f58323be7fd3a1a5e60e1c5
karolinaaa2 <hryhorchuk.karolina@chnu.edu.ua>
2024-10-05 00:34:10 +0300

    Add project files.

---
+++ Ex_3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введіть річний дохід: ");
+        double income = double.Parse(Console.ReadLine());
+        double tax = 0;
+
+        if (income <= 10000)
+        {
+            tax = income * 0.1; // 10% податку
+        }
+        else if (income <= 50000)
+        {
+            tax = income * 0.2; // 20% податку
+        }
+        else
+        {
+            tax = income * 0.3; // 30% податку
+        }
+
+        Console.WriteLine($"Податок на дохід: {tax}");
+    }
+}
